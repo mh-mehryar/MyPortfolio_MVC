34866a76fa1deb1d9c937f2de94af94d9edfe8b5
Mohammad Hossein <mh.mehryar@gmail.com>
2023-05-12 21:25:09 +0330

    MH

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -6,10 +6,38 @@ namespace MyPortfolio.Controllers
 {
     public class HomeController : Controller
     {
+        //private readonly List<MyText> texts = new List<MyText>
+        //{
+        //    new MyText
+        //    {
+        //        Id = 1,Title ="Mohammad",Image="portfolio-1.jpg",Description ="First Text"
+        //    },new MyText
+        //    {
+        //        Id = 2,Title ="Ali",Image="portfolio-2.jpg",Description ="Second Text"
+        //    },new MyText
+        //    {
+        //        Id = 3,Title ="Mina",Image="portfolio-3.jpg",Description ="Third Text"
+        //    },new MyText
+        //    {
+        //        Id = 4,Title ="Maryam",Image="portfolio-4.jpg",Description ="Fourth Text"
+        //    }
+        //};
         
         public IActionResult Index()
         {
+            ViewData["HeadingTitle"] = "MY Portfolio";
+            //return View(texts);
             return View();
+        }
+        public IActionResult Contact()
+        {
+            var contact = new Contact()
+            {
+                Email = "mh.mehryar@outlook.com",
+                Phone = "+98915xxxxxxX",
+                Github = "https://github.com/mh-mehryar"
+            };
+            return View(contact);
         }
 
         public IActionResult Privacy()
@@ -17,10 +45,7 @@ namespace MyPortfolio.Controllers
             return View();
         }
 
-        public IActionResult Shop()
-        {
-            return View();
-        }
+        
        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ MyPortfolio/Models/MyText.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPortfolio.Models
+{
+    public class MyText
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string  Description { get; set; }
+    }
+}
